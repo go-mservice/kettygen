7f07c1b8aaceb667fe688ec96b9d07c3cfe9e8c2
yuyangzi <yuyangzi@meizu.com>
2018-03-07 00:45:16 -0800

    add import

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"os/exec"
+	"fmt"
 )
 
 func goget(url string) {
@@ -20,8 +21,22 @@ func main() {
 	goget("github.com/yyzybb537/protoc-gen-ketty")
 
 	args := []string{"--ketty_out=plugins=:."}
+
+	gopath := os.Getenv("GOPATH")
+	if gopath != "" {
+		args = append([]string{"-I" + gopath + "/src/github.com/yyzybb537/ketty/pbinclude"}, args...)
+		args = append([]string{"-I."}, args...)
+	}
+
 	args = append(args, os.Args[1:]...)
 	cmd := exec.Command("protoc", args...)
+
+	fmt.Printf("$ protoc")
+	for _, arg := range args {
+		fmt.Printf(" %s", arg)
+    }
+	fmt.Printf("\n")
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
